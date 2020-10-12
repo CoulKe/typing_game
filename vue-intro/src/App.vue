a413a2e7ecf9c60ddfec151e48022c3f177a3502
Luteya Coulston <thatcoul@gmail.com>
2020-10-12 21:17:52 +0300

    Initial commit

---
+++ vue-intro/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<p>How fancy are your fingers? </p>
+<div class="data"><span 
+:key="txt.text" v-for="txt in fetchedText"
+>{{ txt }}
+</span></div>
+<button @click="fetchData">Start test</button>
+<textarea name="typingArea" cols="30" rows="10"></textarea>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      inputValue: '',
+      fetchedText: [],
+    }
+  },
+  methods: {
+    fetchData(){
+      fetch('text/free.txt')
+      .then(res => res.text())
+      .then(data => {
+        data.split(' ');
+        this.fetchedText = data
+      })
+    }
+},
+
+};
+</script>
+<style scoped>
+
+</style>
