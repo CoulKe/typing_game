9c62739e1c3303ed864cd193ece4b7d84cfb25c1
Luteya Coulston <thatcoul@gmail.com>
2020-10-13 19:21:09 +0300

    Refactored with async/await

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,63 +1,41 @@
 <template>
-  <p>How fancy are your fingers?</p>
-  <div class="data">
-    <span :key="txt.text" v-for="txt in fetchedText">{{ txt.text }} </span>
+  <h1>How fancy are your fingers?</h1>
+  <div>
+    <span
+    :key="word.position"
+     v-for="word in fetchedText">
+      {{ word.text }} {{ '' }}
+    </span>
   </div>
-  <button @click="fetchData">Start test</button>
-  <textarea
-    name="typingArea"
-    cols="30"
-    rows="10"
-    v-model= "inputValue"
-    @keyup="processInput($event)"
-  ></textarea>
+  <textarea name="" id="" cols="30" rows="10"  :value="inputValue"></textarea>
+  <button @click = fetchData()>Click</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      index: 0,
       inputValue: "",
+      index: 0,
       fetchedText: [],
     };
   },
   methods: {
-    fetchData() {
-      fetch("text/find_you.txt")
-        .then((res) => res.text())
-        .then((data) => {
-          data.split('').forEach(l =>{
-            let ob = {};
-            l === '\n' ? '<br/>' : l;
-            ob['text'] = l;
-            ob['correct'] = false;
-            ob['wrong'] = false;
-            ob['pending'] = true;
-            this.fetchedText.push(ob)
-          })
-        });
+    async fetchData() {
+      const response = await fetch("text/find_you.txt");
+      const data = await response.text();
+      let obj = data.trim().split(" ").map((word, index) => { 
+        return {
+
+            position: index,
+            text: word,
+            correct: false,
+            wrong: false,
+            pending: true
+        }
+     })
+      return this.fetchedText = obj;
     },
-    processInput(event){
-          const value = event.target.value.length;
-
-          if (value === "") {
-              return;
-          }
-          if (this.fetchedText[this.index].text === value.trim().charAt(this.index)) {
-            
-              //correct answer
-              this.fetchedText[this.index].correct = true
-              this.fetchedText[this.index].wrong = false
-              this.fetchedText[this.index].pending = false
-
-              console.log(this.fetchedText[this.index])
-          }
-
-          this.index++
-      }
   },
 };
 </script>
-<style scoped>
-</style>
