e46e23f9d62dd34103f7b5f9415c9a8c9a78ac2d
Luteya Coulston <thatcoul@gmail.com>
2020-10-13 17:31:14 +0300

    Added new logic

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <p>How fancy are your fingers?</p>
   <div class="data">
-    <span :key="txt.text" v-for="txt in fetchedText">{{ txt }} </span>
+    <span :key="txt.text" v-for="txt in fetchedText">{{ txt.text }} </span>
   </div>
   <button @click="fetchData">Start test</button>
   <textarea
     name="typingArea"
     cols="30"
     rows="10"
-    v-model="inputValue"
+    v-model= "inputValue"
     @keyup="processInput($event)"
   ></textarea>
 </template>
@@ -27,37 +27,35 @@ export default {
       fetch("text/find_you.txt")
         .then((res) => res.text())
         .then((data) => {
-          data
-            .trim()
-            .split()
-            .map((key) => {
-              return {
-                text: key,
-                correct: false,
-                wrong: false,
-                pending: true,
-              };
-            });
-          this.fetchedText = data;
+          data.split('').forEach(l =>{
+            let ob = {};
+            l === '\n' ? '<br/>' : l;
+            ob['text'] = l;
+            ob['correct'] = false;
+            ob['wrong'] = false;
+            ob['pending'] = true;
+            this.fetchedText.push(ob)
+          })
         });
     },
     processInput(event){
-      const value = event.target.value
-      let valueIndex = value.length + 1;
+          const value = event.target.value.length;
 
-      if (value === "") {
-        return;
+          if (value === "") {
+              return;
+          }
+          if (this.fetchedText[this.index].text === value.trim().charAt(this.index)) {
+            
+              //correct answer
+              this.fetchedText[this.index].correct = true
+              this.fetchedText[this.index].wrong = false
+              this.fetchedText[this.index].pending = false
+
+              console.log(this.fetchedText[this.index])
+          }
+
+          this.index++
       }
-      if (this.fetchedText[this.index] === value.charAt(valueIndex - 1)) {
-        //correct answer
-        console.log(true)
-              this.keywords[this.index].correct = true
-              this.keywords[this.index].wrong = false
-              this.keywords[this.index].pending = false
-      }
-      this.index++;
-      valueIndex++;
-    }
   },
 };
 </script>
